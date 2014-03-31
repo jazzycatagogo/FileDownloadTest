3268d2753865a4215e066668271d7f73584daaeb
jazzycatagogo <jazzycatagogo@gmail.com>
2014-03-31 22:55:08 +0900

    ファイルダウンロード＋圧縮

--- src/FileDownloadServlet.java
+++ src/FileDownloadServlet.java
@@ -2,6 +2,7 @@
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
+import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.File;
@@ -9,6 +10,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.io.PrintWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -27,13 +29,13 @@ import javax.servlet.http.HttpServletResponse;
 public class FileDownloadServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FileDownloadServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public FileDownloadServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -47,23 +49,68 @@ public class FileDownloadServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-		URL url = new URL("http://localhost:8080/FileDownloadTest/tetsu.jpg");
-		HttpURLConnection con = (HttpURLConnection)url.openConnection();
-		con.setInstanceFollowRedirects(false);
-		con.setAllowUserInteraction(true);
-		con.setRequestMethod("GET");
-		con.connect();
+		//		URL url = new URL("http://localhost:8080/FileDownloadTest/tetsu.jpg");
+		//		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		//		con.setInstanceFollowRedirects(false);
+		//		con.setAllowUserInteraction(true);
+		//		con.setRequestMethod("GET");
+		//		con.connect();
 
 		//String fileName = request.getParameter("FileName");
 
 		//File file = new File("C:\\test\\tetsu.jpg");
 
-		response.setContentType("application/octet-stream");
-		//response.setHeader("Content-disposition", "attachment; filename=\"" + fileName + "\"");
-		response.setContentLength(con.getContentLength());
-		response.setHeader("Expires", "0");
-		response.setHeader("Cache-Control", "must-revalidate, post-check=0,pre-check=0");
-		response.setHeader("Pragma", "private");
+		//		response.setContentType("application/octet-stream");
+		//		response.setHeader("Content-disposition", "attachment; filename=\"tetsu.jpg\"");
+		//		response.setContentLength(con.getContentLength());
+		//		response.setHeader("Expires", "0");
+		//		response.setHeader("Cache-Control", "must-revalidate, post-check=0,pre-check=0");
+		//		response.setHeader("Pragma", "private");
+
+		//		int status = con.getResponseCode();
+		//
+		//		if(status != HttpURLConnection.HTTP_OK){
+		//			throw new ServletException();
+		//		}
+
+		//		ServletOutputStream os = response.getOutputStream();
+		//		BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
+		//
+		//	     byte[] buff = new byte[500];
+		//	     int len;
+		//	     while ((len = bis.read(buff)) > 0) {
+		//	       os.write(buff, 0, len);
+		//	     }
+		//	     bis.close();
+		//	     os.flush();
+
+		//		DataInputStream ins = new DataInputStream(con.getInputStream());
+		//		DataOutputStream ous = new DataOutputStream(
+		//				new BufferedOutputStream(
+		//						new FileOutputStream("C:\\test\\tetsu.jpg")));
+
+		//PrintWriter writer = response.getWriter();
+
+		//		byte[] b = new byte[4096];
+		//		int readByte = 0;
+		//
+		//
+		//		while(-1 != (readByte = ins.read(b))){
+		//			ous.write(b, 0, readByte);
+		//		}
+		//		ins.close();
+		//		ous.close();
+
+		//String fileName = request.getParameter("FileName");
+
+		//File file = new File("C:\\test\\tetsu.jpg");
+		// httpではディレクトリ以下指定ができない仕様
+		// すべてのファイルを取得するにはすべてのファイル名を知る必要がある
+		// FTPやNFSなら全ファイルのリストが取れそう？
+		URL url = new URL("http://localhost:8080/FileDownloadTest/tetsu.jpg");
+		HttpURLConnection con = (HttpURLConnection)url.openConnection();
+		con.setRequestMethod("GET");
+		con.connect();
 
 		int status = con.getResponseCode();
 
@@ -71,53 +118,36 @@ public class FileDownloadServlet extends HttpServlet {
 			throw new ServletException();
 		}
 
-//		ServletOutputStream os = response.getOutputStream();
-//		BufferedInputStream bis = new BufferedInputStream(con.getInputStream());
-//
-//	     byte[] buff = new byte[500];
-//	     int len;
-//	     while ((len = bis.read(buff)) > 0) {
-//	       os.write(buff, 0, len);
-//	     }
-//	     bis.close();
-//	     os.flush();
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		ZipOutputStream zos = new ZipOutputStream(baos);
 
-		DataInputStream ins = new DataInputStream(con.getInputStream());
-		DataOutputStream ous = new DataOutputStream(
-				new BufferedOutputStream(
-						new FileOutputStream("C:\\test\\tetsu.jpg")));
-
-		//PrintWriter writer = response.getWriter();
-
-		byte[] b = new byte[4096];
-		int readByte = 0;
-
-
-		while(-1 != (readByte = ins.read(b))){
-			ous.write(b, 0, readByte);
+		ZipEntry ze = new ZipEntry("./tetsu/tetsu.jpg");
+		zos.putNextEntry(ze);
+		int len = con.getContentLength();
+		byte[] buf = new byte[len];
+		DataInputStream dis = new DataInputStream(con.getInputStream());
+		for (int i = 0; i < len; i++) {
+			buf[i] = dis.readByte();
 		}
-		ins.close();
-		ous.close();
+		zos.write(buf, 0, len);
+		dis.close();
 
-	     File zipf = new File("tetsu.zip");
-	     File[] files = { new File("C:\\test")};
-	     ZipOutputStream zos = new ZipOutputStream(new FileOutputStream(zipf));
+		zos.close();
 
-	     for(File f : files){
-	    	 ZipEntry ze = new ZipEntry(f.getPath().replace("\\\\", "/"));
-	    	 zos.putNextEntry(ze);
-	    	 InputStream is = new BufferedInputStream(new FileInputStream(f));
-	    	 for(;;){
-					int len = is.read(b);
-					if (len < 0) break;
-					zos.write(b, 0, len);
-				}
-				is.close();
-	     }
+		response.setContentType("application/zip");
+		// filenameはDBから取ってきた値をセットする
+		response.setHeader("Content-disposition", "attachment; filename=\"tetsu.zip\"");
+		response.setContentLength(baos.size());
+
+		//PrintWriter out = response.getOutputStream();
+		OutputStream os = response.getOutputStream();
+		os.write(baos.toByteArray());
+		System.err.println("size: " + baos.size());
 
 
-
-
+		if(con != null){
+			con.disconnect();
+		}
 	}
 
 }
